32f9728e4dfcdb74879f2118a6f0f355a6483f9b
Matthias Abraham <matthias.abraham@gmail.com>
2022-12-09 11:24:51 +0100

    Beautify progress reporting

--- scripts/sys_win/caffeinate.ps1
+++ scripts/sys_win/caffeinate.ps1
@@ -4,10 +4,15 @@ Write-Output "Caffeinating for $minutes minutes..."
 
 $myshell = New-Object -com "Wscript.Shell"
 
-for ($i = 0; $i -lt $minutes; $i++) {
-  Write-Output "Sleeping for 60 seconds... zzZ"
-  Start-Sleep -Seconds 60
-  Write-Output "Zzz Caffein booster"
+$interval = 10 # seconds
+$loops = 60 * $minutes / $interval
+
+for ($i = 0; $i -lt $loops; $i++) {
+  $progressPercent = 100 * $i / $loops
+  $secondsRemaining = ($loops - $i) * $interval
+  $status = "Infusing keystrokes... {0,4:n1}%" -f $progressPercent
+  Write-Progress -Activity "Caffeinate" -Status $status -SecondsRemaining $secondsRemaining -PercentComplete $progressPercent
+  Start-Sleep -Seconds $interval
   $myshell.sendkeys("{NUMLOCK}")
   $myshell.sendkeys("{NUMLOCK}")
 }
